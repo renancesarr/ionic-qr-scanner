d1cbf3799a6110add21b0f4141a1baedbd3d9626
Renan <stdio@MacBook-Pro-de-Renan.local>
2021-02-05 16:59:59 -0300

    inicio projeto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,49 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  scanSub: any;
+  qrText: string
 
-  constructor() {}
+  constructor(public platform: Platform, private qrScanner: QRScanner) {
+    this.platform.backButton.subscribeWithPriority(0, () => {
+      document.getElementById('qr-code-scanner').style.opacity = '1';
+    });
+  }
+  startScanning() {
+    // Optionally request the permission early
+    this.qrScanner.prepare().
+      then((status: QRScannerStatus) => {
+        if (status.authorized) {
+          this.qrScanner.show();
+          const divQrCode = document.getElementById("leitorqr");
+          divQrCode.removeChild(divQrCode.firstChild);
+          const imgQrCode = document.createElement("img");
+          imgQrCode.src = "../../assets/Ativo 24.png";
+          divQrCode.appendChild(imgQrCode);
+          document.getElementById('qr-code-scanner').style.opacity = '0.2';
+          document.getElementById('container').style.opacity = '0.6';
+          document.getElementById('leitorqr').style.opacity = '1';
+
+          this.scanSub = this.qrScanner.scan()
+            .subscribe((textFound: string) => {
+              document.getElementById('qr-code-scanner').style.opacity = '1';
+              this.qrScanner.hide();
+              this.scanSub.unsubscribe();
+
+              this.qrText = textFound;
+              console.log(this.qrText);
+
+            }, (err) => {
+              debugger;
+              console.log(err)
+            });
+        } else if (status.denied) {
+        } else {
+
+        }
+      })
+      .catch((e: any) => console.log('Error is', e));
+  }
+
 
 }
